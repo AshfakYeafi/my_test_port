57ab82e8dc1ef14ae657e384a52d51c376db9385
Ashfak Yeafi <ashfakyeafifahim@gmail.com>
2023-11-26 01:13:28 +0600

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import { withContentlayer } from 'next-contentlayer'
+
+export default withContentlayer({
+    images: {
+        domains: ['images.unsplash.com', 'cdn.buymeacoffee.com'],
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        scrollRestoration: true,
+        mdxRs: false,
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+})
